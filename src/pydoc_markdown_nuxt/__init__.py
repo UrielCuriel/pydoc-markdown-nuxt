da0a1098489d46071b7c22fb88d68d09b6b518dd
Uriel Curiel <u.curiel@kyati.mx>
2025-07-25 11:29:00 -0600

    chore(release): version 0.1.1

--- src/pydoc_markdown_nuxt/__init__.py
+++ src/pydoc_markdown_nuxt/__init__.py
@@ -5,36 +5,33 @@ This package provides a renderer for pydoc-markdown that generates documentation
 following the Nuxt Content and MDC (Markdown Components) structure.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Uriel Curiel"
 __email__ = "urielcuriel@outlook.com"
 
-from .renderer import NuxtRenderer, NuxtPage
+from .renderer import NuxtPage, NuxtRenderer
 from .utils import (
+    NuxtContentHelper,
+    create_api_overview_table,
     create_mdc_alert,
     create_mdc_code_group,
     create_mdc_tabs,
-    create_mdc_variables,
-    create_mdc_arguments,
-    create_variable_or_argument_component,
     create_navigation_entry,
     enhance_frontmatter_for_nuxt,
-    generate_api_breadcrumbs,
     format_python_signature_for_mdc,
-    create_api_overview_table,
-    NuxtContentHelper
+    generate_api_breadcrumbs,
 )
 
 __all__ = [
     "NuxtRenderer",
     "NuxtPage",
     "create_mdc_alert",
-    "create_mdc_code_group", 
+    "create_mdc_code_group",
     "create_mdc_tabs",
     "create_navigation_entry",
     "enhance_frontmatter_for_nuxt",
     "generate_api_breadcrumbs",
     "format_python_signature_for_mdc",
     "create_api_overview_table",
-    "NuxtContentHelper"
+    "NuxtContentHelper",
 ]
